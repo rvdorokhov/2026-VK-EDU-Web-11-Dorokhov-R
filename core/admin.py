83981a13aab138624c8344f00ffcf90fe10c7ac6
rvdorokhov <theonlywaytotravel@mail.ru>
2026-04-30 21:38:35 +0300

    Сделал кастомного user для бд

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import User, Profile
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+        "email",
+        "is_staff",
+        "is_active",
+    )
+
+    search_fields = (
+        "username",
+        "email",
+    )
+
+    list_filter = (
+        "is_staff",
+        "is_active",
+    )
+
+    class ProfileInline(admin.TabularInline):
+        model = Profile
+
+        fields = (
+            "nickname",
+            "avatar",
+        )
+
+        extra = 0
+
+    inlines = (ProfileInline,)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "nickname",
+        "avatar",
+
+        "user__id",
+        "user__email",
+    )
+
+    list_select_related = (
+        "user",
+    )
+
+    raw_id_fields = (
+        "user",
+    )
+
+    search_fields = (
+        "user__id",
+        "user__email",
+
+        "nickname",
+    )
--- core/models.py
+++ core/models.py
@@ -1,1 +1,34 @@
 from django.db import models
+from django.conf import settings
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(verbose_name="Почта", max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name="Пользователь", on_delete=models.CASCADE, )
+
+    nickname = models.CharField(verbose_name="Никнейм", max_length=64)
+
+    avatar = models.ImageField(
+        verbose_name="Фотография профиля",
+        upload_to="avatars/",
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = "Профиль пользователя"
+        verbose_name_plural = "Профили пользователей"
+
+    def __str__(self):
+        return self.nickname
